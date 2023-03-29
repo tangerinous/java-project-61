c934f940e63e60434f2df78f97122441fbe6fedc
David Chaava <david.chaava@akvelon.com>
2023-03-29 17:06:19 +0200

    com1

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import java.util.Objects;
 import java.util.Scanner;
 
-import static java.lang.System.exit;
 import static java.lang.System.out;
 
 public class Engine {
@@ -14,27 +13,46 @@ public class Engine {
 
     private final String name;
 
+    /**
+     * @param scanner
+     * @param maxCorrectAnswers
+     * @param name
+     */
     public Engine(Scanner scanner, int maxCorrectAnswers, String name) {
         this.scanner = scanner;
         this.maxCorrectAnswers = maxCorrectAnswers;
         this.name = name;
     }
 
+    /**
+     * @return
+     */
     public boolean shouldContinue() {
         return correctAnswers < maxCorrectAnswers;
     }
 
+    /**
+     * @param question
+     * @return
+     */
     public String askQuestion(Object question) {
         out.println("Question: " + question);
         out.println("Your answer: ");
         return scanner.next();
     }
 
+    /**
+     *
+     */
     public void correctAnswer() {
         out.println("Correct!");
         correctAnswers++;
     }
 
+    /**
+     * @param answer
+     * @param correct
+     */
     public void notCorrectAnswer(String answer, String correct) {
         out.printf("""
                 '%s' is wrong answer ;(. Correct answer was '%s'.
@@ -43,6 +61,10 @@ public class Engine {
         correctAnswers = Integer.MAX_VALUE;
     }
 
+    /**
+     * @param answer
+     * @param correctAnswer
+     */
     public void checkAnswer(String answer, String correctAnswer) {
         if (Objects.equals(answer, correctAnswer)) {
             correctAnswer();
