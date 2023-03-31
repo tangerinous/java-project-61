55b7ea5e13a677d96c0947dbe4287bd7aab3a57c
David Chaava <david.chaava@akvelon.com>
2023-03-31 14:27:29 +0200

    fix checkstyle

--- app/src/main/java/hexlet/code/games/GcdGame.java
+++ app/src/main/java/hexlet/code/games/GcdGame.java
@@ -22,6 +22,9 @@ public class GcdGame {
         int max = Integer.max(left, right);
         int min = Integer.min(left, right);
 
+        if(min == 0)
+            return 1;
+
         while (min > 1) {
             int mod = max % min;
             if (mod == 0) {
